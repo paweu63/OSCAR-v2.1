94594667e21b02ca80e367b508f7b6f2c6a57812
Pawel <pawel.furmanek@onet.pl>
2026-02-17 20:07:42 +0100

    Added declarations

---
+++ src/decalarations.h
@@ -0,0 +1,15 @@
+#define VeloPin1 35
+#define VeloPin2 39
+#define VeloPin3 34
+#define VeloPin4 100 //to be changed
+#define VeloPin5 36 
+#define ButtonMove 14
+#define ButtonPump 27
+#define PumpPin 25
+#define SERVO_PIN_1 12
+#define SERVO_PIN_2 13
+#define LedPin1 33
+#define LedPin2 26
+#define LedPin3 17
+#define LedPin4 16
+#define LedPin5 15
